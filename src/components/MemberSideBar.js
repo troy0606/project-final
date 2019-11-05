99ce30efc6b0e8b5e8fb7629476bec874e7be79d
BoAn Chen <troy0606troy0606@gmail.com>
2019-11-05 16:24:05 +0800

    position

--- src/components/MemberSideBar.js
+++ src/components/MemberSideBar.js
@@ -1,41 +1,42 @@
-import React, { Component } from 'react'
-import '../common/member_sideBar.css'
+import React, { Component } from "react";
+import "../common/member_sideBar.css";
 
 const MemberSideBar = () => {
-    return (
-        <div className="container-fluid d-flex">
-        <aside className="side-bar"  style={{minHeight:'1080px'}}>
-            <div className="member-info">
-                <figure className="member-photo">
-                    <img src="https://pixel.nymag.com/imgs/daily/vulture/2015/04/02/02-winnie-the-pooh.w330.h330.jpg"
-                        className="object" alt="..."/>
-                </figure>
-                <h5 className="member-title text-center">LIU HAN WEN</h5>
-            </div>
-            <div className="member-function d-flex flex-column align-items-center">
-                <div className="member-profile">
-                    <p>PROFILE</p>
-                    <h3>基本資料修改</h3>
-                    <h3>重設密碼</h3>
-                </div>
-                <div className="member-like">
-                    <p>LIKED AND　WATCHED</p>
-                    <h3>收藏推薦清單</h3>
-                    <h3>瀏覽紀錄</h3>
-                </div>
-                <div className="member-order">
-                    <p>ACTIVITY FINISHING</p>
-                    <h3>優惠活動</h3>
-                    <h3>訂單紀錄</h3>
-                </div>
-                <div className="text-center mt-5">
-                    <h3>Home</h3>
-                    <h3>SIgn Out</h3>
-                </div>
-            </div>
-        </aside>
-    </div>
-    )
-}
+  return (
+    <aside className="side-bar d-flex flex-column" style={{ height: "100%" }}>
+      <div className="member-info">
+        <figure className="member-photo">
+          <img
+            src="https://pixel.nymag.com/imgs/daily/vulture/2015/04/02/02-winnie-the-pooh.w330.h330.jpg"
+            className="object"
+            alt="..."
+          />
+        </figure>
+        <h5 className="member-title text-center">LIU HAN WEN</h5>
+      </div>
+      <div className="member-function d-flex flex-column align-items-center">
+        <div className="member-profile">
+          <p>PROFILE</p>
+          <h3>基本資料修改</h3>
+          <h3>重設密碼</h3>
+        </div>
+        <div className="member-like">
+          <p>LIKED AND　WATCHED</p>
+          <h3>收藏推薦清單</h3>
+          <h3>瀏覽紀錄</h3>
+        </div>
+        <div className="member-order">
+          <p>ACTIVITY FINISHING</p>
+          <h3>優惠活動</h3>
+          <h3>訂單紀錄</h3>
+        </div>
+        <div className="text-center mt-5">
+          <h3>Home</h3>
+          <h3>SIgn Out</h3>
+        </div>
+      </div>
+    </aside>
+  );
+};
 
-export default MemberSideBar
+export default MemberSideBar;
