0fb20f320530d285128dd47f3e17af0e713fc356
html.css practice <50762485+abc820219@users.noreply.github.com>
2019-11-06 22:35:36 +0800

    add

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,29 +5,48 @@ import { GiHamburgerMenu } from "react-icons/gi";
 import NavBarSign from "./NavBarSign";
 import NavBarUnSign from "./NavBarUnSign";
 import MemberLogin from "./MemberLogin";
+import { DropdownButton } from "react-bootstrap";
+import { read } from "fs";
+import { relative } from "path";
 const NavBar = props => {
   const [signIn] = useState(false);
   const [showLightBox, setShowLightBox] = useState(false);
+  const [showMenuBtn, setshowMenuBtn] = useState(false);
   const memberSignIn = () => {
     setShowLightBox(!showLightBox);
+  };
+  const MenuList = () => {
+    setshowMenuBtn(!showMenuBtn);
   };
   if (showLightBox) {
     console.log("clicked");
   }
   return (
     <>
-
       <nav className="navbar page-nav d-flex">
         <div className="page-nav-aside d-flex align-items-center justify-content-center">
-          <GiHamburgerMenu className="mx-3 " style={{ fontSize: "30px" }} />
+          <div style={{ position: relative }} onClick={MenuList}>
+            <GiHamburgerMenu className="mx-3 " style={{ fontSize: "30px" }} />
+            {showMenuBtn ? (
+              <ul className="navbarBtn">
+                <li>課程</li>
+                <li>老師</li>
+                <li>店家</li>
+              </ul>
+            ) : null}
+          </div>
           <h3>BAKE TIME</h3>
         </div>
-        {signIn ? <NavBarSign /> : <NavBarUnSign showLightBox={memberSignIn}/>}
+        {signIn ? <NavBarSign /> : <NavBarUnSign showLightBox={memberSignIn} />}
       </nav>
-      {showLightBox ? <MemberLogin LoginBox={signIn}  memberSignIn={memberSignIn}></MemberLogin> : null} 
+      {showLightBox ? (
+        <MemberLogin
+          LoginBox={signIn}
+          memberSignIn={memberSignIn}
+        ></MemberLogin>
+      ) : null}
     </>
   );
-
 };
 
 export default NavBar;
