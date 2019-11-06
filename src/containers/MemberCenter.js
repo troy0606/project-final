849fd3dd55283047a0f9ae172ed9600fd1a3b03e
apple <apple@apple.local>
2019-11-06 17:50:55 +0800

    add

--- src/containers/MemberCenter.js
+++ src/containers/MemberCenter.js
@@ -2,21 +2,18 @@ import React from "react";
 import MemberOrder from "../components/MemberOrder";
 import MemberSideBar from "../components/MemberSideBar";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-// import { Row, Col ,Container} from "react-bootstrap"
+import { Row, Col ,Container} from "react-bootstrap"
 const MemberCenter = ({ match }) => {
   return (
     <>
-      <div className="d-flex container-fuild">
-          <div className="row">
+      <div className="d-flex">
         <Router>
-
           <MemberSideBar />
           <Switch>
             <Route path={`/member/:order`} component={MemberOrder} />
           </Switch>
         </Router>
 
-          </div>
       </div>
     </>
   );
